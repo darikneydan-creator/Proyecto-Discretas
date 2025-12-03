ebbef9ffea8c878c3fcfa2d396c9fb2e36af903b
Cybel <iagohb@ciencias.unam.mx>
2025-12-03 03:12:26 -0600

    Terminamos creo

--- README.md
+++ README.md
@@ -1,23 +1,114 @@
+Implementamos los arboles n-arios de la siguiente forma:
 
-    Hernández Bautista Iago
-        No. de Cuenta: 323256223
-    Omana Ramos Darik Neydan
-        No. de Cuenta: 322211175
-    Solorio Rauda Pedro
-        No. de Cuenta: 323112343
+```data ArbolN a = Void | Node a [ArbolN a]```
+
+El tipo de dato que elegimos es un nodo con una lista de hijos.
+
+Decidimos usar este tipo de dato porque las listas permiten que cada nodo tenga una cantidad arbitraria de arboles n-arios hijos.
+Además de tener cualquier cantidad de hijos, las listas nos permiten conservar el orden de los nodos. No es lo mismo tener este arbol:
+```
+  A
+ / \
+B   C
+   / \
+  D   E
+```
+a tener este arbol:
+```
+    A
+   / \
+  C   B
+ / \
+E   D
+```
+Por si solo puede no parecer útil, pero sí importa cuando nos encontramos con funciones que requieren orden (como preorden y postorden).
+
+Y la última razón por la que elegimos usar listas para manejar a los hijos es porque ya las habíamos visto en clase, entonces estamos más familiarizados con cómo funcionan y cómo usarlas.
+
+Aunque usamos listas para hacer los árboles n-arios, también tienen desventajas. Por ejemplo, como los árboles pueden estar sueltos o en listas, nos forzó a crear funciones auxiliares dentro de cada función principal para tratar con árboles sueltos y para tratar con listas de árboles.
+
+Además, como un árbol puede ser vacío, existen dos formas de representar un nodo sin hijos. Una forma es con una lista vacía
+
+``` Node x [] ```
+
+y otra forma es con una lista de arboles vacíos
+
+``` Node x [Void] ```
+
+No afecta a la hora de contar elementos o trabajar con las listas, porque una lista de árboles vacía y un árbol vacío son tratados casi igual, pero no es muy elegante tener dos formas de representar la misma cosa, puede causar ambigüedad y confusión
 
 _____________________________________________________________________
-Tipo de dato usado:
-Void | Node a [ArbolN a]
 
-Este tipo de dato es un nodo con una lista de hijos, la razón de usar
-este dato es porque queremos que cada nodo tenga cualquier número de hijos,
-incluyendo la lista vacía, donde los hijos o elementos de la lista, también son
-árboles n-arios, y además, no queremos que tenga dos listas (izq y der) 
-como hijas, ya que esto no tendría ningún sentido, nos complicaría la lectura
-y además podemos abarcar todos los hijos en una sola lista. Una desventaja de
-este dato (probablemente, inevitable) es que nos forzó a elaborar funciones
-auxiliares dentro y fuera de las principales, por el mismo cambio en el tipo de
-dato que entraba y salía, una leía árboles, mientras que la otra leía arreglos
-de árboles.
+# LISTA DE FUNCIONES:
 
+## ÁRBOLES DE SINTAXIS ABSTRACTA:
+
+- ```propToArbol :: Prop -> ArbolSintax```
+
+    Función que convierte una proposición lógica a su árbol de sintaxis
+
+- ```arbolToProp :: ArbolSintax -> Prop```
+
+    Función que recibe un árbol de sintaxis y devuelve una proposición
+
+- ```interpretacionArbol :: ArbolSintax -> Estado -> Bool```
+
+    Función que recibe un árbol de sintaxis, un estado de variables y devuelve la interpretación del árbol
+
+
+## OTRAS FUNCIONES:
+
+- ```cantidadElementos :: ArbolN a -> Int```
+
+    Función que cuenta todos los elementos de un árbol n-ario
+
+- ```busca :: a -> ArbolN a -> Bool```
+
+    Función que busca si un elemento pertenece a un árbol n-ario
+
+- ```sumaElementos :: ArbolN Int -> Int```
+
+    Función que toma un árbol n-ario de enteros y devuelve la suma de sus elementos
+
+- ```preorden :: ArbolN a -> [a]```
+
+    Función que toma un árbol n-ario y regresa una lista de sus elementos según los encuentra en recorrido en preorden
+
+- ```postorden :: ArbolN a -> [a]```
+
+    Función que toma un árbol n-ario y regresa una lista de sus elementos según los encuentra en recorrido en postorden
+
+- ```altura :: ArbolN a -> Int```
+
+    Función que toma un árbol n-ario y regresa la altura de dicho árbol
+
+- ```espejo :: ArbolN a -> ArbolN a```
+
+    Función que invierte las ramas de un árbol n-ario. Las de la izquierda van a la derecha y viceversa
+
+- ```podar :: Int -> ArbolN a -> ArbolN a```
+
+    Función que toma un nivel de profundidad y corta las ramas de un árbol a partir de ahí
+
+- ```elementosProfundidad :: Int -> ArbolN a -> [a]```
+
+    Función que devuelve la lista de elementos en una profundidad x de un árbol n-ario
+
+_____________________________________________________________________
+
+# Miembros del equipo:
+- Hernández Bautista Iago
+
+    No. de cuenta: ```323256223```
+
+- Omana Ramos Darik Neydan
+
+    No. de cuenta: ```322211175```
+
+- Solorio Rauda Pedro
+
+    No. de cuenta: ```323112343```
+
+_____________________________________________________________________
+# Info adicional:
+Dentro de ```proyecto.hs``` hay varios arboles (t1 a t9) y proposiciones (p1 a p3) para que sea más fácil probar las funciones. Si descarga el repositorio, puede editar ```proyecto.hs``` para agregar, cambiar o eliminar las plantillas que tenemos.
--- proyecto.hs
+++ proyecto.hs
@@ -66,7 +66,7 @@ arbolToProp (Node "→" [p, q]) = Impl (arbolToProp p) (arbolToProp q)
 arbolToProp (Node "↔" [p, q]) = Syss (arbolToProp p) (arbolToProp q)
 
 -- Funcion que recibe un arbol de sintaxis, un estado de variables y devuelve la interpretacion del arbol
-interpretacionArbol :: ArbolSintax -> Estado ->  Bool
+interpretacionArbol :: ArbolSintax -> Estado -> Bool
 interpretacionArbol (Node "True" []) _ = True
 interpretacionArbol (Node "False" []) _ = False
 interpretacionArbol (Node p []) xs  =  if esElemento p xs then True else False
@@ -82,163 +82,80 @@ interpretacionArbol (Node "↔" [p, q]) xs = bicondicional (interpretacionArbol 
 --   SECCION 2: OTRAS FUNCIONES   --
 ------------------------------------
 
-numeroElementos :: ArbolN a -> Int
-numeroElementos Void = 0
-numeroElementos (Node tx hijos) = 1 + contarHijos hijos
-
---Función auxiliar para: numeroElementos
---Cuenta cuantos hijos tiene una lista
-contarHijos :: [ArbolN a] -> Int
-contarHijos [] = 0
-contarHijos (t:ts) = numeroElementos t + contarHijos ts
---para que sume todos los hijos de una lista trees [tree,tree..]
--- contarHijos [t1,t2,t3] = numeroElementos t1 + numeroElementos t2 + numeroElementos t3
-
-{- -- Mi implementacion de numeroElementos (iago)
 -- Funcion que cuenta todos los elementos de un arbol n-ario
 cantidadElementos :: ArbolN a -> Int
 cantidadElementos Void = 0
-cantidadElementos (Node _ lista) = 1 + cantidadElementosHijos lista
+cantidadElementos (Node _ hijos) = 1 + cantidadElementosHijos hijos
     where
         cantidadElementosHijos :: [ArbolN a] -> Int
         cantidadElementosHijos [] = 0
         cantidadElementosHijos (x:xs) = cantidadElementos x + cantidadElementosHijos xs
--}
 
-busca :: Eq a => ArbolN a -> a -> Bool
-busca Void _ = False
-busca (Node tx hijos) a = if(tx==a) then True
-                          else buscaHijos hijos a
-
---Función auxiliar para: busca
---Busca en la lista de hijos a ver si en los hijos se encuentra el elemento
-buscaHijos :: Eq a => [ArbolN a] -> a -> Bool
-buscaHijos [] _ = False
-buscaHijos (Void:ts) a = buscaHijos ts a
-buscaHijos ((Node x hijos):ts) a = if x == a then True
-                                   else buscaHijos hijos a || buscaHijos ts a
-                                   --para que busque en la lista de hijos de una rama y en los que estoy parado.
---deseo saber el valor a (x) del arbol actual, por lo tanto especifico (Node x hijos) : ts que es cada elemento
-
-{- Mi implementacion de busca (iago)
 -- Funcion que busca si un elemento pertenece a un arbol n-ario
 busca :: Eq a => a -> ArbolN a -> Bool
 busca _ Void = False
-busca a (Node b lista) = if (a == b) then True else buscaHijos a lista
+busca a (Node b hijos) = if (a == b) then True else buscaHijos a hijos
     where
         buscaHijos :: Eq a => a -> [ArbolN a] -> Bool
         buscaHijos _ [] = False
         buscaHijos a (x:xs) = disyuncion (busca a x) (buscaHijos a xs)
--}
 
-sumaElementos :: Num a => ArbolN a -> a
-sumaElementos Void = 0
-sumaElementos (Node tx hijos) = tx + sumarHijos hijos
-
---Función auxiliar para: sumaElementos
---Obtiene la suma de los nodos hijos
-sumarHijos :: Num a => [ArbolN a] -> a
-sumarHijos [] = 0
-sumarHijos (t:ts) = sumaElementos t + sumarHijos ts
-
-{- Mi implementacion de sumaElementos (iago)
 -- Funcion que toma un arbol n-ario de enteros y devuelve la suma de sus elementos
 sumaElementos :: ArbolN Int -> Int
 sumaElementos Void = 0
-sumaElementos (Node x lista) = x + sumaElementosHijos lista
+sumaElementos (Node x hijos) = x + sumaElementosHijos hijos
     where
         sumaElementosHijos :: [ArbolN Int] -> Int
         sumaElementosHijos [] = 0
         sumaElementosHijos (x:xs) = sumaElementos x + sumaElementosHijos xs
--}
 
+-- Funcion que toma un arbol n-ario y regresa una lista de sus elementos segun los encuentra en recorrido en preorden
 preorden :: ArbolN a -> [a]
 preorden Void = []
-preorden (Node tx hijos) = [tx]++(recorrepreorden hijos)
-
---Función auxiliar para: preorden
---Agrega los recorridos de una lista de hijos a la lista de recorrido agregando primero los de la izquierda
-recorrepreorden :: [ArbolN a] -> [a]
-recorrepreorden [] = []
-recorrepreorden (t:ts) = (preorden t)++(recorrepreorden ts)
-
-{- Mi implementacion de preorden (iago)
--- Funcion que toma un arbol n-ario y regresa una lista de sus elementos, empezando por las ramas izquierdas y luego por las derechas
-preorden :: ArbolN a -> [a]
-preorden Void = []
-preorden (Node a []) = [a]
-preorden (Node a (x:xs)) = [a] ++ preorden x ++ preordenHijos xs
+preorden (Node a hijos) = [a] ++ preordenHijos hijos
     where
         preordenHijos :: [ArbolN a] -> [a]
         preordenHijos [] = []
         preordenHijos (x:xs) = preorden x ++ preordenHijos xs
--}
 
--- Funcion que toma un arbol n-ario y regresa una lista de sus elementos, empezando por las ramas derechas y luego por las izquierdas
+-- Funcion que toma un arbol n-ario y regresa una lista de sus elementos segun los encuentra en recorrido en postorden
+
 postorden :: ArbolN a -> [a]
 postorden Void = []
-postorden (Node a []) = [a]
-postorden (Node a (x:xs)) = [a] ++ postordenHijos xs ++ postorden x
+postorden (Node a hijos) = postordenHijos hijos ++ [a]
     where
         postordenHijos :: [ArbolN a] -> [a]
         postordenHijos [] = []
-        postordenHijos (x:xs) = postordenHijos xs ++ postorden x
+        postordenHijos (x:xs) = postorden x ++ postordenHijos xs
 
-altura :: ArbolN a -> Int
-altura Void = 0
-altura (Node tx hijos) = 1 + alturaHijos hijos
-
---Función auxiliar para: altura
---Obtiene el máximo de las alturas de una lista de hijos, ya sea en la que estoy parado u otro en la lista
-alturaHijos :: [ArbolN a] -> Int
-alturaHijos [] = 0
-alturaHijos (t:ts) = maximo (altura t)(alturaHijos ts)
-
-{- Mi implementacion de altura (iago)
 -- Funcion que toma un arbol n-ario y regresa la altura de dicho arbol
 altura :: ArbolN a -> Int
 altura Void = 0
-altura (Node a []) = 1
-altura (Node a (x:xs)) = maximo (1 + altura x) (1 + alturaHijos xs)
+altura (Node a hijos) = 1 + alturaHijos hijos
     where
         alturaHijos :: [ArbolN a] -> Int
         alturaHijos [] = 0
         alturaHijos (x:xs) = maximo (altura x) (alturaHijos xs)
--}
 
-espejo :: ArbolN a -> ArbolN a --ya que no cambia el tipo de dato
-espejo Void = Void
-espejo (Node tx hijos) = (Node tx (invierte hijos))
-
---Función auxiliar para: espejo
---Invierte una lista de arboles (Los hijos de cada nodo padre)
-invierte :: [ArbolN a] -> [ArbolN a]
-invierte [] = []
-invierte (t:ts) = snoc (invierte(ts)) (espejo t)
-
-{- Mi implementacion de espejo (iago)
 -- Funcion que invierte las ramas de un arbol n-ario. Las de la izquierda van a la derecha y viceversa
 espejo :: ArbolN a -> ArbolN a
 espejo Void = Void
-espejo (Node a []) = (Node a [])
-espejo (Node a lista) = (Node a (espejoHijos lista))
+espejo (Node a hijos) = (Node a (espejoHijos hijos))
     where
         espejoHijos :: [ArbolN a] -> [ArbolN a]
         espejoHijos [] = []
-        espejoHijos (x:xs) = espejoHijos xs ++ [espejo x]
--}
+        espejoHijos (x:xs) = snoc (espejoHijos xs) (espejo x)
 
--- Funcion que corta ramas de un arbol
+-- Funcion que toma un nivel de profundidad y corta las ramas de un arbol a partir de ahi
 podar :: Int -> ArbolN a -> ArbolN a
-podar 0 _ = Void
 podar _ Void = Void
-podar _ (Node a []) = (Node a []) -- El numero no puede ser 0 por la primera regla, entonces tiene que ser minimo 1. Y como este arbol solo tiene 1 nivel, el resultado es el mismo arbol
-podar y (Node a (x:xs)) = (Node a ([podar (y-1) x] ++ (podarHijos (y-1) xs)))
+podar 0 _ = Void
+podar y (Node a hijos) = (Node a (podarHijos (y-1) hijos))
     where
         podarHijos :: Int -> [ArbolN a] -> [ArbolN a]
-        podarHijos 0 _ = []
         podarHijos _ [] = []
-        podarHijos y (x:xs) = [podar (y-1) x] ++ (podarHijos (y-1) xs)
+        podarHijos 0 _ = []
+        podarHijos y (x:xs) = [podar (y) x] ++ (podarHijos (y) xs)
 
 -- Funcion que devuelve la lista de elementos en una profundidad x de un arbol n-ario
 elementosProfundidad :: Int -> ArbolN a -> [a]
@@ -251,59 +168,76 @@ elementosProfundidad y (Node _ lista) = elementosProfundidadHijos (y-1) lista
         elementosProfundidadHijos 0 (x:xs) = (elementosProfundidad 0 x) ++ (elementosProfundidadHijos 0 xs)
         elementosProfundidadHijos y (x:xs) = (elementosProfundidad (y) x) ++ (elementosProfundidadHijos (y) xs)
 
+
+
 ------------------------------
 --   FUNCIONES AUXILIARES   --
 ------------------------------
 
---Funcion maximo
+-- Funcion maximo
 maximo :: Int -> Int -> Int
 maximo x y = if x > y then x else y
 
---Funcion snoc
+-- Funcion snoc
 snoc :: [a] -> a -> [a]
 snoc [] b = [b]
 snoc (x:xs) b = x:(snoc xs b)
 
+-- Funcion disyuncion
 disyuncion :: Bool -> Bool -> Bool
 disyuncion False False = False
 disyuncion _ True = True
 disyuncion True _ = True
 
+-- Funcion negacion
 negacion :: Bool -> Bool
 negacion True = False
 negacion False = True
 
+-- Funcion conjuncion
 conjuncion :: Bool -> Bool -> Bool
 conjuncion True True = True
 conjuncion _ False = False
 conjuncion False _ = False
 
+-- Funcion implicacion
 implicacion :: Bool -> Bool -> Bool
 implicacion True True = True
 implicacion False _ = True
 implicacion True False = False
 
+-- Funcion bicondicional
 bicondicional :: Bool -> Bool -> Bool
 bicondicional True True = True
 bicondicional False False = True
 bicondicional True False = False
 bicondicional False True = False
 
+-- Funcion esElemento
 esElemento :: Eq a => a -> [a] -> Bool --Funcion esElemento devuelve True si x es elemento de lista xs, False si no es elemento
 esElemento _ [] = False
 esElemento x (y:xs) = if (x == y) then True else (esElemento x xs)
 
 
------------------
---   ARBOLES   --
------------------
 
-{-
+--------------------------------
+--   ARBOLES Y PROPOSICIONES  --
+--------------------------------
+
 t1 :: ArbolN Int
-t1 = Node 1 [Node 2 [], Node 3 []]
+t1 = Node 1 [
+        Node 2 [],
+        Node 3 []
+    ]
 
 t2 :: ArbolN Int
-t2 = Node 10 [Void, Node 20 [Node 30 []], Void]
+t2 = Node 10 [
+        Void,
+        Node 20 [
+            Node 30 []
+        ],
+        Void
+    ]
 
 t3 :: ArbolN Int
 t3 = Node 1 [
@@ -315,6 +249,7 @@ t3 = Node 1 [
             ]
         ]
     ]
+
 t4 :: ArbolN Int
 t4 = Node 0 [
         Node 1 [],
@@ -327,6 +262,7 @@ t4 = Node 0 [
         Node 8 [],
         Node 9 []
     ]
+
 t5 :: ArbolN Int
 t5 = Node 100 [
         Node 50 [
@@ -348,26 +284,106 @@ t5 = Node 100 [
             Void
         ]
     ]
+
 t6 :: ArbolN Int
 t6 = Void
 
--- Árbol del diagrama:
-
 t7 :: ArbolN Char
-t7 =
-  Node 'a'
-    [ Node 'b'
-        [ Node 'e'
-            [ Node 'j' []
-            , Node 'k' []
+t7 = Node 'a' [
+        Node 'b' [
+            Node 'e' [
+                Node 'j' [],
+                Node 'k' []
             ]
-        ]
-    , Node 'c' []
-    , Node 'd'
-        [ Node 'f' []
-        , Node 'g' []
-        , Node 'h' []
-        , Node 'i' []
+        ],
+        Node 'c' [],
+        Node 'd' [
+            Node 'f' [],
+            Node 'g' [],
+            Node 'h' [],
+            Node 'i' []
         ]
     ]
+
+t8 :: ArbolN Char
+t8 = Node 'A' [
+        Node 'B' [
+            Node 'D' [
+                Node 'H' [],
+                Node 'I' []
+            ],
+            Node 'E' [
+                Node 'J' [],
+                Node 'K' []
+            ]
+        ],
+        Node 'C' [
+            Node 'F' [
+                Node 'L' [],
+                Node 'M' []
+            ],
+            Node 'G' [
+                Node 'N' [],
+                Node 'O' []
+            ]
+        ]
+    ]
+
+p1 :: Prop
+p1 =
+  Impl
+    (And
+       (Or (Var "p") (Not (Var "q")))
+       (Impl (Var "r") (And (Var "s") (Not (Var "t"))))
+    )
+    (Syss
+       (Or (Var "u") (Var "v"))
+       (And (Not (Var "w")) (Impl (Var "x") (Var "y")))
+    )
+
+p2 :: Prop
+p2 =
+  And
+    (Not (Impl (Var "a") (Not (Var "b"))))
+    (Or
+       (Impl
+          (And (Var "c") (Var "d"))
+          (Not (Or (Var "e") (Var "f")))
+       )
+       (Syss
+          (Impl (Var "g") (Var "h"))
+          (And (Not (Var "i")) (Not (Var "j")))
+       )
+    )
+
+p3 :: Prop
+p3 =
+  Syss
+    (And
+       (Or (Var "p1") (Var "p2"))
+       (And
+          (Impl (Var "p3") (Or (Var "p4") (Var "p5")))
+          (Not (And (Var "p6") (Var "p7")))
+       )
+    )
+    (Impl
+       (Or
+          (And (Var "p8") (Not (Var "p9")))
+          (Impl (Var "p10") (Var "p11"))
+       )
+       (And (Var "p12") (Not (Var "p13")))
+    )
+
+
+{-
+Miembros de equipo:
+
+Hernández Bautista Iago
+    No. de cuenta: 323256223
+
+Omana Ramos Darik Neydan
+    No. de cuenta: 322211175
+
+Solorio Rauda Pedro
+    No. de cuenta: 323112343
 -}
